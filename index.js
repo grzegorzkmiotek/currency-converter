2606a9fdeb85741bf74a2aba303246dc166110b7
[Grzegorz Kmiotek] <grzegorz.kmiotek+grupa41@gmail.com>
2023-10-09 20:53:25 +0200

    corrected

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
+const errorLabel = document.getElementById("error");
+
 async function fetchExchangeRate(currencyCode) {
 	const url = `https://api.nbp.pl/api/exchangerates/rates/a/${currencyCode}`;
-	const response = await fetch(url);
+	try {
+		const response = await fetch(url);
 
-	if (!response.ok) {
-		throw new Error(`Nie udało się...${response.statusText}`);
+		if (!response.ok) {
+			throw new Error(`Nie udało się...${response.statusText}`);
+		}
+		const data = await response.json();
+		return data?.rates?.[0]?.mid;
+	} catch {
+		throw new Error(`Błąd Api`);
 	}
-	const data = await response.json();
-	return data.rates[0].mid;
 }
 
 function convertCurrency(event) {
 	event.preventDefault();
+	errorLabel.innerText = "";
 	const code = document.querySelector("#from-currency").value;
 	const amount = document.querySelector("#amount").value;
-	// if (rate) {
-	// 	const result = rate * amount;
-	// 	document.querySelector("#result").innerText = result.toFixed(2);
-	// } else {
-	// 	// wyświetlenie błędu użytkownikowi
-	// }
 
 	fetchExchangeRate(code)
 		.then((rate) => {
-			const result = rate * amount;
-			document.querySelector("#result").innerText = result.toFixed(2);
+			if (rate) {
+				const result = rate * amount;
+				document.querySelector("#result").innerText = result.toFixed(2);
+			} else {
+				errorLabel.innerText = "Wystąpił błąd";
+			}
 		})
-		.catch((error) => {
-			console.error(`Wystąpił błąd! ${error}`);
-			// document.createElement("li").innerText =
-			// ''`Wystąpił błąd! ${error}`spróbuj później...'
+		.catch(() => {
+			errorLabel.innerText = "Wystąpił błąd";
 		});
-	return data?.rates?.[0]?.mid;
 }
 document
 	.querySelector("#income-form")
